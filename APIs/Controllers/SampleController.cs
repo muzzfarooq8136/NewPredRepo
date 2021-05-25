48756b087929b0524143c5700b0ac1686887fc78
Muzammil Farooq <muzammil.farooq@intagleo.com>
2021-05-25 15:17:59 +0500

    Add project files.

---
+++ APIs/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using APIs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<ResponseModel> Get()
+        {
+            var response = new ResponseModel();
+            response.ResponseMessage = "Welcome to Predatar!";
+            return response;
+        }
+    }
+}
